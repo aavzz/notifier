6f38e0b7c2613b2c24ca9d27c891623ab7b6116e
aavzz <aavzz@yandex.ru>
2017-12-20 10:05:51 +0400

    Update cfgfile.go

--- setup/cfgfile.go
+++ setup/cfgfile.go
@@ -11,29 +11,18 @@ import (
 	"os"
 )
 
-type smtpSection struct {
-	address string
-	port    uint16
-}
-
 type beelineSection struct {
-	url      string
 	login    string
 	password string
 }
 
 type configurationFile struct {
-	smtp    smtpSection
 	beeline beelineSection
 }
 
 var cfgFile configurationFile
 
 func readConfig() {
-
-	cfgFile.smtp.address = "localhost"
-	cfgFile.smtp.port = 25
-	cfgFile.beeline.url = "https://beeline.amega-inform.ru/sendsms/"
 
 	cfg, err := ini.Load(cmdLnOpts.cfgfile)
 
@@ -42,17 +31,14 @@ func readConfig() {
 		os.Exit(1)
 	}
 
-	_, err = cfg.GetSection("smtp")
+	_, err = cfg.GetSection("beeline")
 
 	if err == nil {
-		if cfg.Section("smtp").HasKey("address") {
-			cfgFile.smtp.address = cfg.Section("smtp").Key("address").String()
+		if cfg.Section("beeline").HasKey("login") {
+			cfgFile.beeline.login = cfg.Section("beeline").Key("login").String()
 		}
-		if cfg.Section("smtp").HasKey("port") {
-			p, err := cfg.Section("smtp").Key("port").Uint()
-			if err == nil {
-				cfgFile.smtp.port = uint16(p)
-			}
+		if cfg.Section("beeline").HasKey("password") {
+			p, err := cfg.Section("beeline").Key("password").String()
 		}
 	}
 
