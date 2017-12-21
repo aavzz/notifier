b2ab595ecb1f5beb796535696d573a26ee85c86f
aavzz <aavzz@yandex.ru>
2017-12-21 05:04:25 +0400

    Update main.go

--- main.go
+++ main.go
@@ -14,19 +14,6 @@ func main() {
 
 	Setup()
 	defer RemovePidFile()
-	SysLog.Info("starting daemon...")
+	SysLog.Info("Successfull start")
 
-	//just some event loop
-	//newer mind that it does not do anything useful 
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(ConfigAddress(), nil)
-	if err != nil {
-		SysLog.Err(err.Error())
-		os.Exit(1)
-	}
-
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	SysLog.Info("Hello World!")
 }
