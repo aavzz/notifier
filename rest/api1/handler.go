8c0f3b8fce7a3fa9ee08c70dc1f104ea3c3b8eed
aavzz <aavzz@yandex.ru>
2017-12-21 13:51:29 +0400

    Update handler.go

--- rest/api1/handler.go
+++ rest/api1/handler.go
@@ -15,8 +15,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		case "beeline":
 			re := regexp.MustCompile(`+7\d{10}`)
 			phones := re.FindAllString(recipients, 5)
-			msg := message[:480]
-
+			l := len(message)
+			if l > 480 {
+				l = 480
+			}
+			msg := message[:l]
+			if {
+			
+			}
 		case "email":
 		
 		case "telegram":
