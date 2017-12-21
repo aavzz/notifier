6d39f074de76952f91f83fa1315fe2df134c0aff
aavzz <aavzz@yandex.ru>
2017-12-21 07:24:51 +0400

    Update beeline.go

--- rest/api1/beeline.go
+++ rest/api1/beeline.go
@@ -1,5 +1,20 @@
 package api1
 
 include (
-
+  "fmt"
+  "github.com/gorilla/http"
 )
+
+func sendMessage(numbers string, message string) {
+  c := new(http.Client) 
+
+  status, _, r, err := c.Post("http://www.example.com", nil, "qwertyu")
+  if err != nil {
+    SysLog.Err(err.Error())
+  }
+  if r != nil {
+    defer r.Close()
+  }
+  SysLog.Info(fmt.Sprintf("Post result: %v", status))
+  
+}
