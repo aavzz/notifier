a71a2e889742c60f90f1af181612b914eeaac7de
aavzz <aavzz@yandex.ru>
2017-12-21 08:26:32 +0400

    Update beeline.go

--- rest/api1/beeline.go
+++ rest/api1/beeline.go
@@ -2,13 +2,24 @@ package api1
 
 include (
   "fmt"
+  "net/url"
   "github.com/gorilla/http"
 )
 
 func sendMessage(numbers string, message string) {
-  c := new(http.Client) 
+    
+  parameters := url.Values{}
+  parameters.Add("user", "")
+  parameters.Add("pass", "")
+  parameters.Add("action", "post_sms")
+  parameters.Add("target", numbers)
+  parameters.Add("message", message)
+  
+  //Content-Type: application/x-www-form-urlencoded; charset=windows-1251
+  
+  c := new(http.Client)
 
-  status, _, r, err := c.Post("https://beeline.amega-inform.ru/sendsms/", nil, "qwertyu")
+  status, _, r, err := c.Post("https://beeline.amega-inform.ru/sendsms/", nil, parameters.Encode())
   if err != nil {
     SysLog.Err(err.Error())
   }
