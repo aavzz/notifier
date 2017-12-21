216853cbffd3176d1a3e377910d1763d39e5f08a
aavzz <aavzz@yandex.ru>
2017-12-21 13:23:45 +0400

    Update handler.go

--- rest/api1/handler.go
+++ rest/api1/handler.go
@@ -2,9 +2,29 @@ package api1
 
 import (
 	"fmt"
+	"regexp"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	channel := r.FormValue("channel")
+	recipients := r.FormValue("recipients")
+	message := r.FormValue("message")
+
+	switch channel {
+		case "beeline":
+			re := regexp.MustCompile("+7\d{10},")
+		//check recipients
+		//check message (480 symbols)
+		case "email":
+		
+		case "telegram":
+
+		default:
+		
+	}
+	
+	
+	
 	fmt.Fprintf(w, "Hello World!")
 }
