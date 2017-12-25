cf2600c31b25ec95b376ab1a0f8871da6788902c
aavzz <aavzz@yandex.ru>
2017-12-25 12:23:27 +0400

    Update beeline.go

--- rest/api1/beeline.go
+++ rest/api1/beeline.go
@@ -7,42 +7,56 @@ import (
 	"net/http"
 	"crypto/tls"
 	"io/ioutil"
+	"golang.org/x/text/encoding/charmap"
 	. "github.com/aavzz/notifier/setup/syslog"
 	. "github.com/aavzz/notifier/setup/cfgfile"
+	
 )
 
 func sendMessageBeeline(numbers string, message string) {
   
-	parameters := url.Values{
-		"user": {ConfigBeelineLogin()},
-		"pass": {ConfigBeelinePassword()},
-		"sender": {ConfigBeelineSender()},
-		"action": {"post_sms"},
-		"target": {numbers},
-		"message": {message},
-	}
-  
-	req, err := http.NewRequest("POST", "https://beeline.amega-inform.ru/sendsms/", strings.NewReader(parameters.Encode())) 
+	c, err := CfgFileContent()
 	if err != nil {
-		SysLog.Err(err.Error())
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=windows-1251")
-	
-	tr := &http.Transport{
-        	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	c := &http.Client{Transport: tr}
+		SysLog.Err(err.Error())	
+	} else {
+		msg, err := charmap.Windows1251.NewEncoder().String(message)
+		if err != nil {
+			SysLog.Err(err.Error())	
+		} else {
+			parameters := url.Values{
+				"user": {*c.beeline.login},
+				"pass": {*c.beeline.password},
+				"sender": {*c.beeline.sender},
+				"action": {"post_sms"},
+				"target": {numbers},
+				"message": {msg},
+			}
 
-  	resp, err := c.Do(req)
-	if err != nil {
-		SysLog.Err(err.Error())
+			url := "https://beeline.amega-inform.ru/sendsms/"
+			req, err := http.NewRequest("POST", url, strings.NewReader(parameters.Encode())) 
+			if err != nil {
+				SysLog.Err(err.Error())
+			}
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=windows-1251")
+	
+			tr := &http.Transport{
+		        	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+			c := &http.Client{Transport: tr}
+
+		  	resp, err := c.Do(req)
+			if err != nil {
+				SysLog.Err(err.Error())
+			}
+			if resp != nil {
+				defer resp.Body.Close()
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			bodyString := string(body)
+			SysLog.Info(fmt.Sprintf("Post result: %v", resp.Status))
+			SysLog.Info(fmt.Sprintf("Post result: %v", bodyString))
+			SysLog.Info(fmt.Sprintf("Post result: %v", strings.NewReader(parameters.Encode())))
+
+		}
 	}
-	if resp != nil {
-		defer resp.Body.Close()
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	bodyString := string(body)
-	SysLog.Info(fmt.Sprintf("Post result: %v", resp.Status))
-	SysLog.Info(fmt.Sprintf("Post result: %v", bodyString))
-	SysLog.Info(fmt.Sprintf("Post result: %v", strings.NewReader(parameters.Encode())))
 }
