fe6a1df07150336fa1f02ff0e36388dd60890b13
aavzz <aavzz@yandex.ru>
2017-12-25 21:16:26 +0400

    Update handler.go

--- rest/api1/handler.go
+++ rest/api1/handler.go
@@ -16,18 +16,15 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	switch channel {
 		case "beeline":
 			re := regexp.MustCompile(`\+7\d{10}`)
-			SysLog.Info("BBBBBBB")
 			phones := strings.Join(re.FindAllString(recipients, 5), ",")
 			l := len(message)
 			if l > 480 {
 				l = 480
 			}
 			msg := message[:l]
-			SysLog.Info("CCCC")
-			SysLog.Info(phones)
 			if phones != "" && msg != "" {
-				sendMessageBeeline(phones, msg)
 				SysLog.Info(fmt.Sprintf("Message '%s' sent via beeline to %s", msg, phones))
+				sendMessageBeeline(phones, msg)
 			} else {
 				SysLog.Info(fmt.Sprintf("Failed to send message via beeline"))
 			}
@@ -38,5 +35,4 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		default:
 			SysLog.Info("No valid channel found")	
 	}
-		
 }
