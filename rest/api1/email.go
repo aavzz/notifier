309837e52dfa5c51a5999829307112514813e5b8
aavzz <aavzz@yandex.ru>
2017-12-26 18:27:01 +0400

    Update email.go

--- rest/api1/email.go
+++ rest/api1/email.go
@@ -1,25 +1,25 @@
 package api1
 
 import (
-  "net/smtp"
-  "gopkg.in/gomail.v2"
-  . "github.com/aavzz/notifier/setup/syslog"
+	"net/smtp"
+	"gopkg.in/gomail.v2"
+	. "github.com/aavzz/notifier/setup/syslog"
 	. "github.com/aavzz/notifier/setup/cfgfile"
 )
 
 func sendMessageEmail(emails []string, message string) {
  
-  m := gomail.NewMessage()
-  m.SetHeaders(map[string][]string{
-    "From":    {m.FormatAddress("alex@example.com", "Alex")},
-    "To":      emails,
-    "Subject": {"Hello"},
-  })
-  m.SetBody("text/plain", "Hello!")
+	m := gomail.NewMessage()
+	m.SetHeaders(map[string][]string{
+		"From":    {m.FormatAddress("alex@example.com", "Alex")},
+		"To":      emails,
+		"Subject": {"Hello"},
+	})
+	m.SetBody("text/plain", "Hello!")
   
-  d := gomail.Dialer{Host: "localhost", Port: 25}
-  if err := d.DialAndSend(m); err != nil {
-    SysLog.Err(err.Error())
-  }
+	d := gomail.Dialer{Host: "localhost", Port: 25}
+	if err := d.DialAndSend(m); err != nil {
+		SysLog.Err(err.Error())
+	}
   
 }
