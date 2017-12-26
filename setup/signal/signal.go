052cd1b7a1a8e31e0bd0cb956422d30837e04313
aavzz <aavzz@yandex.ru>
2017-12-26 11:25:37 +0400

    Update signal.go

--- setup/signal/signal.go
+++ setup/signal/signal.go
@@ -11,23 +11,15 @@ import (
 
 func SignalHandling() {
 
-	sigusr1 := make(chan os.Signal, 1)
 	sighup := make(chan os.Signal, 1)
 	sigint := make(chan os.Signal, 1)
 	sigquit := make(chan os.Signal, 1)
 	sigterm := make(chan os.Signal, 1)
 
-	signal.Notify(sigusr1, syscall.SIGUSR1)
 	signal.Notify(sighup, syscall.SIGHUP)
 	signal.Notify(sigint, syscall.SIGINT)
 	signal.Notify(sigquit, syscall.SIGQUIT)
 	signal.Notify(sigterm, syscall.SIGTERM)
-
-	go func() {
-		<-sigusr1
-		SysLog.Info("SIGUSR1 received")
-		os.Exit(-1)
-	}()
 
 	go func() {
 		for {
