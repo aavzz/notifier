1651f8ebe3fdd5f391d00378fa1282cffb23f17e
aavzz <aavzz@yandex.ru>
2017-12-26 18:23:26 +0400

    Update email.go

--- rest/api1/email.go
+++ rest/api1/email.go
@@ -1,5 +1,23 @@
 package api1
 
 import (
-
+  "net/smtp"
+  "gopkg.in/gomail.v2"
 )
+
+func sendMessageEmail(emails []string, message string) {
+ 
+  m := gomail.NewMessage()
+  m.SetHeaders(map[string][]string{
+    "From":    {m.FormatAddress("alex@example.com", "Alex")},
+    "To":      emails,
+    "Subject": {"Hello"},
+  })
+  m.SetBody("text/plain", "Hello!")
+  
+  d := gomail.Dialer{Host: "localhost", Port: 25}
+  if err := d.DialAndSend(m); err != nil {
+    SysLog.Err(err.Error())
+  }
+  
+}
