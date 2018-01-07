985cda29496ebb7b5f082bcbabd1b65056df82e7
aavzz <aavzz@yandex.ru>
2018-01-07 20:44:52 +0400

    Update main.go

--- client/notify/main.go
+++ client/notify/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"github.com/aavzz/notifier/client/notify/cmd"
+	"os"
+	"log"
 )
 
 func main() {
+	//check if we are invoked as a pipe
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatal(err.Error())	
+	}
+	if fi.Mode() & os.ModeNamedPipe == 0 {
+		log.Fatal("stdin not connected to pipe")
+	}
+	
 	cmd.Execute()
 }
