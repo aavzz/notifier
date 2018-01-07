4933ddaa80aea2aed6da8f69954ecb5f5f30399c
aavzz <aavzz@yandex.ru>
2018-01-07 20:46:24 +0400

    Update root.go

--- client/notify/cmd/root.go
+++ client/notify/cmd/root.go
@@ -23,20 +23,16 @@ func notifyCommand(cmd *cobra.Command, args []string) {
 // Execute starts notify execution
 func Execute() {
         beeline.Flags().StringP("recipients", "r", "", "recipient list")
-        beeline.Flags().StringP("message", "m", "", "message to send")
         beeline.Flags().StringP("url", "u", "", "url to query")
 	viper.BindPFlag("beeline.recipients", beeline.Flags().Lookup("recipients"))
-	viper.BindPFlag("beeline.message", beeline.Flags().Lookup("message"))
 	viper.BindPFlag("beeline.url", beeline.Flags().Lookup("url"))
 
         email.Flags().StringP("recipients", "r", "", "recipient list")
-        email.Flags().StringP("message", "m", "", "message to send")
         email.Flags().StringP("sender-name", "n", "", "sender name")
         email.Flags().StringP("sender-address", "a", "", "sender address")
         email.Flags().StringP("subject", "s", "", "email subject")
         email.Flags().StringP("url", "u", "", "url to query")
 	viper.BindPFlag("email.recipients", email.Flags().Lookup("recipients"))
-	viper.BindPFlag("email.message", email.Flags().Lookup("message"))
 	viper.BindPFlag("email.sender-name", email.Flags().Lookup("sender-name"))
 	viper.BindPFlag("email.sender-address", email.Flags().Lookup("sender-address"))
 	viper.BindPFlag("email.subject", email.Flags().Lookup("subject"))
