c649116c3a355496865e1c5b67cf821e0dd72f76
aavzz <aavzz@yandex.ru>
2018-02-01 22:08:30 +0400

    Update notifier.go

--- notifier.go
+++ notifier.go
@@ -26,6 +26,11 @@ type NotifierResponse struct {
 //message - message to send
 func NotifySMS(notifyd, channel, phones, message string) error {
 
+	log.Info(notifyd)
+	log.Info(channel)
+	log.Info(phones)
+	log.Info(message)
+	
 	params := url.Values{
 		"channel":    {channel},
 		"recipients": {phones},
